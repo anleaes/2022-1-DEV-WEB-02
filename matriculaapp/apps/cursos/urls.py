e37ebe402aaeb7fe41d50a6b9eaa0a58dc338640
Marcelo Abbi <marcelo.abbi@outlook.com>
2022-06-25 23:49:10 -0300

    criacao do app cursos

---
+++ matriculaapp/apps/cursos/urls.py
@@ -0,0 +1,8 @@
+app_name = 'cursos'
+
+urlpatterns = [
+    path('', views.list_cursos, name='list_cursos'),
+    path('adicionar/', views.add_category, name='add_category'),
+    path('editar/<int:id_category>/', views.edit_category, name='edit_category'),
+    path('excluir/<int:id_category>/', views.delete_category, name='delete_category'),
+]
--- matriculaapp/matriculaapp/urls.py
+++ matriculaapp/matriculaapp/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('cursos/', include('cursos.urls', namespace='cursos')),
 
 ]
 
